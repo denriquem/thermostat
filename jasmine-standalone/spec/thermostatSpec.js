e15dedfed4fe65e685c76dfb64f6bf4f5dcd60d6
Daniel <demartinezgatell@gmail.com>
2019-12-12 10:58:33 +0000

    displaying usage levels

--- jasmine-standalone/spec/thermostatSpec.js
+++ jasmine-standalone/spec/thermostatSpec.js
@@ -1,13 +1,15 @@
 'use strict';
 
-describe('Thermostat', function(){
-  var thermostat;
-beforeEach(function(){
-  thermostat = new Thermostat();
-});
+describe('Thermostat', function() {
 
-  it('starts at 20 degrees', function(){
-    expect(thermostat.getCurrentTemperature().toEqual(20);
+  var thermostat;
+
+  beforeEach(function() {
+  thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
   it('increases in temperature with up()', function() {
@@ -32,7 +34,7 @@ beforeEach(function(){
   });
 
   it('can switch PSM off', function(){
-    thermostat.swithPowerSavingModeOff();
+    thermostat.switchPowerSavingModeOff();
     expect(thermostat.isPowerSavingModeOn()).toBe(false);
   });
 
@@ -43,7 +45,15 @@ beforeEach(function(){
     expect(thermostat.isPowerSavingModeOn()).toBe(true);
   });
 
-});
+  it('can be reset to the default temperature', function() {
+      for (var i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      thermostat.resetTemperature();
+      expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+
 
 describe('when power saving mode is on', function(){
   it('has a temperature of 25 degrees', function(){
@@ -54,12 +64,41 @@ describe('when power saving mode is on', function(){
   });
 });
 
-describe('when power saving mode is off', function() {
-  it('has a maximum temperature of 35 degrees', function(){
-    thermostat.switchPowerSavingModeOff();
-    for (var i = 0; i < 13, i++){
+describe('when power saving mode is on', function() {
+  it('has a maximum temperature of 25 degrees', function() {
+    for (var i = 0; i < 6; i++) {
       thermostat.up();
     }
-    expect(thermostat.getCurrentTemperature()).toEqual(32);
+    expect(thermostat.getCurrentTemperature()).toEqual(25);
   });
+});
+
+describe('displaying usage levels', function(){
+  describe('when the temperature is below 18 degrees', function(){
+    it('it is considered low-usage', function(){
+      for (var i = 0; i < 3; i++){
+        thermostat.down();
+      }
+      expect(thermostat.energyUsage()).toEqual('low-usage');
+    });
+  });
+
+  describe('when the temperature is between 18 and 25 degrees', function() {
+    it('it is considered medium-usage', function() {
+      expect(thermostat.energyUsage()).toEqual('medium-usage');
+    });
+  });
+
+  describe('when the temperature is anything else', function() {
+    it('it is considered high-usage', function() {
+      thermostat.powerSavingMode = false;
+      for (var i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.energyUsage()).toEqual('high-usage');
+    });
+
+  });
+});
+
 });
